c1d67be3a0f0e248a1cae205903be5fa8b071c2b
Lasto <ejo3t965ek@gmail.com>
2019-04-23 22:42:17 +0800

    Create error.php

---
+++ error.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Sign in &middot; Twitter Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="http://localhost/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+      .jumbotron {
+        margin: 60px 0;
+        text-align: center;
+      }
+
+    </style>
+    <link href="http://localhost/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://localhost/bootstrap/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://localhost/bootstrap/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://localhost/bootstrap/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://localhost/bootstrap/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="http://localhost/bootstrap/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="http://localhost/bootstrap/ico/favicon.png">
+  </head>
+
+  <body>
+    <hr>
+      <div class="jumbotron">
+        <h1>Error</h1>
+        <p class="lead">Fail connection: your account or password is invalid</p>
+        <a class="btn btn-large btn-primary" href="index.php">Back to login</a>
+      </div>
+    <hr>
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://localhost/bootstrap/js/jquery-2.0.3.min.js"></script>
+    <script type="text/javascript" src="http://localhost/bootstrap/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
